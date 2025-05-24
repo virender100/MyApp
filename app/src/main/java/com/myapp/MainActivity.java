e9dfb13688ea45b53019873814d7631f37b5a629
virender100 <virendersingh15012001@gmail.com>
2025-05-24 21:04:50 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, world!");
+        setContentView(tv);
+    }
+}
